3acac83a6daec5fecb0c839c7706866dd2dff3ea
Kunkanya Remmer <kunkanyaremmer@hotmail.com>
2021-11-15 16:08:39 +0100

    V5:retrived unique ingredients, appareils and ustensils data

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
 import {recipes} from "./recipes.js"
+//--DOM
+const btnIngredients = document.getElementById("searchByIngredients")
+const tagIngredients = document.getElementById("dropdownIngredients")
 
-console.log(recipes)
+let ingredients = []
+let arrIngredients = [] 
+let appareils = []
+let arrAppareils =[]
+let ustensiles = []
+let arrUtensiles =[]
 
+//--get all Ingredients, Appareils, Ustensiles
+recipes.forEach(recipe =>{
+    recipe.ingredients.forEach(ingredient =>{
+        arrIngredients.push(ingredient.ingredient)
+        //--filter duplicate ingredients
+        ingredients = [...new Set(arrIngredients)]
+        })
+
+        //--get and filter duplicate appareils 
+        arrAppareils.push(recipe.appliance)
+        appareils =[...new Set(arrAppareils)]
+  
+    //--get and filter duplicate ustensiles
+    recipe.ustensils.forEach(ustensil =>{
+        arrUtensiles.push(ustensil)
+        ustensiles =[...new Set(arrUtensiles)]    
+    })
+})
+console.log(ingredients)
+console.log(appareils)
+console.log(ustensiles)
+
+btnIngredients.addEventListener('click', ()=>{
+    
+    tagIngredients.innerHTML = `  
+    ${ingredients.map(function(ingredient){
+        return `<li>${ingredient}</li>`
+    }).join('')}
+    `   
+})
